6c2139a7a259f779adda48b64e54601a457406a4
Arci <araskind819@gmail.com>
2021-10-12 02:36:24 +0700

    service

---
+++ app/Services/ReportService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\DB;
+use Carbon\CarbonPeriod;
+
+class ReportService
+{
+  public $data;
+
+  public $outputArray;
+
+  public function __construct()
+  {
+    $period = new CarbonPeriod('2021-09-01', '2021-10-01');
+    
+    foreach ($period as $date) {
+      $this->outputArray[] = $date->format('Y-m-d');
+    }
+  }
+
+  public function getReport() {
+    $this->data = DB::table('orders');
+    $this->outputArray = array_fill_keys($this->outputArray, []);
+
+    foreach ($this->data->get() as $row) {
+      if(isset($this->outputArray[$row->end_at])) {
+        $this->outputArray[$row->end_at][] = $row;
+      }
+    }
+  }  
+}
